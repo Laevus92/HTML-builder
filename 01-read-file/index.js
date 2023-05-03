b4d8b0758fcede27e8d91c7a2ddc4cfb684af56e
Laevus92 <laevus@yandex.ru>
2023-05-03 18:20:38 +0300

    refactor: corrected code in compliance with eslint

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-const path = require('path')
-let data = ''
+const path = require('path');
+let data = '';
 const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 stream.on('data', chunk => data += chunk);
 stream.on('end', () => {
-  console.log(data)
-})
+  console.log(data);
+});
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,43 +1,43 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
+const { stdin: input} = require('node:process');
 const filename = path.join(__dirname, 'text.txt');
 
 function openFile() {
   fs.open(filename, 'w', (err) => {
-    if (err) throw err
-  })
+    if (err) throw err;
+  });
   fs.writeFile(filename, '', (err) => {
-    if (err) throw err
-  })
+    if (err) throw err;
+  });
 }
 
 
 function writeToFile(data) {
   fs.appendFile(filename, (data + '\n'), (err) => {
-    if (err) throw err
-  })
+    if (err) throw err;
+  });
 }
 
 function runApp() {
   const rl = readline.createInterface({ input, });
   rl.question('', (answer) => {
     if (answer !== 'exit') {
-      writeToFile(answer)
-      runApp()
+      writeToFile(answer);
+      runApp();
     } else {
       console.log('Your text was added in file!\nSee you mate!');
-      process.exit()
+      process.exit();
     }
-  })
+  });
 }
-runApp()
-openFile()
+runApp();
+openFile();
 
 console.log('You created text file.\nInput your text below:');
 
 process.on('SIGINT', () => {
   console.log('Your text was added in file!\nSee you mate!');
-  process.exit()
-})
+  process.exit();
+});
