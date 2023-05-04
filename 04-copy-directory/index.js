3df860be1ba9e7a39fc5dbbef2b533ddf32d3f6e
Laevus92 <laevus@yandex.ru>
2023-05-04 17:21:23 +0300

    fix: fixed 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,33 @@
-const { constants } = require('buffer');
 const fs = require('fs/promises');
 const path = require('path');
-const files = fs.readdir(path.join(__dirname, 'files'));
+const sourceDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
 
-async function createFolder() {
-  await fs.mkdir(path.join(__dirname, 'files-copy'), {recursive : true} );
-}
-async function copyPaste() {
-  (await files).forEach(file => {
-    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), constants.COPYFILE_EXCL);
-  });
+async function copyDir(source, target) {
+
+  try {
+    await fs.rmdir(target, { recursive: true });
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err;
+  }
+
+  await fs.mkdir(target);
+
+  const files = await fs.readdir(source);
+
+  for (const file of files) {
+    const sourcePath = path.join(source, file);
+    const targetPath = path.join(target, file);
+
+    const stat = await fs.stat(sourcePath);
+
+    if (stat.isDirectory()) {
+      await copyDir(sourcePath, targetPath);
+    } else {
+      await fs.copyFile(sourcePath, targetPath);
+    }
+  }
+  console.log('__SUCCESSFUL!__');
 }
 
-createFolder();
-copyPaste();
-console.log('__SUCCSESS__');
+copyDir(sourceDir, targetDir);
