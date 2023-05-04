5f67b56e15973db38a662adc9740ac858cec5672
Laevus92 <laevus@yandex.ru>
2023-05-04 11:32:30 +0300

    feat: resolved task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const { constants } = require('buffer');
+const fs = require('fs/promises');
+const path = require('path');
+const files = fs.readdir(path.join(__dirname, 'files'));
 
+async function createFolder() {
+  await fs.mkdir(path.join(__dirname, 'files-copy'), {recursive : true} );
+}
+async function copyPaste() {
+  (await files).forEach(file => {
+    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), constants.COPYFILE_EXCL);
+  });
+}
+
+createFolder();
+copyPaste();
+console.log('__SUCCSESS__');
