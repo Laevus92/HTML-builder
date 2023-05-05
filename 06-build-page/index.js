0ae43195d8d57d42cbe60912bcc9f8b8f3e5fa19
Laevus92 <laevus@yandex.ru>
2023-05-05 16:09:50 +0300

    feat: resolved 06-build-page task

---
+++ 06-build-page/index.js
@@ -0,0 +1,134 @@
+const fs = require('fs');
+const path = require('path');
+
+async function copyDir(source, target) {
+
+  try {
+    await fs.promises.rmdir(target, { recursive: true });
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err;
+  }
+
+  await fs.promises.mkdir(target);
+
+  const files = await fs.promises.readdir(source);
+
+  for (const file of files) {
+    const sourcePath = path.join(source, file);
+    const targetPath = path.join(target, file);
+
+    const stat = await fs.promises.stat(sourcePath);
+
+    if (stat.isDirectory()) {
+      await copyDir(sourcePath, targetPath);
+    } else {
+      await fs.promises.copyFile(sourcePath, targetPath);
+    }
+  }
+}
+
+async function mergeFiles(sourceDir, targetFile) {
+  //remove bundle.css
+  try {
+    await fs.promises.unlink(targetFile);
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err;
+  }
+
+  //create new style.css
+  await fs.promises.open(targetFile, 'w');
+  let data = '';
+  const files = await fs.promises.readdir(sourceDir, {withFileTypes : true});
+  for (const file of files) {
+    if (file.isFile() && path.extname(file['name']) === '.css') {
+      data = '';
+      const stream = fs.createReadStream(path.join(sourceDir, file['name']), 'utf-8');
+      stream.on('data', (chunk) => {
+        data += chunk;
+      });
+      stream.on('end', () => {
+        fs.appendFile(targetFile, data, err => {
+          if (err) throw err;
+        });
+      });
+    }
+  }
+}
+
+async function buildPage() {
+  //remove project-dist folder
+  try {
+    await fs.promises.rmdir(path.join(__dirname, 'project-dist'), { recursive: true });
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err;
+  }
+  // //create new project-dist folder
+  await fs.promises.mkdir(path.join(__dirname, 'project-dist'));
+  //copy assets
+  const assetsSource = path.join(__dirname, 'assets');
+  const assetsTarget = path.join(__dirname, 'project-dist', 'assets');
+  copyDir(assetsSource, assetsTarget);
+
+  // merge style.css
+  const mergedStyle = path.join(__dirname, 'project-dist', 'style.css');
+  const sourseOfStyles = path.join(__dirname, 'styles');
+  mergeFiles(sourseOfStyles, mergedStyle);
+  //create object with components
+  let componentsObject = {};
+  const componentsSource = path.join(__dirname, 'components');
+  const components = await fs.promises.readdir(componentsSource, { withFileTypes: true });
+  const indexHtmlTarget = path.join(__dirname, 'project-dist', 'index.html');
+  function createComponentObject() {
+    return new Promise((res, rej) => {
+      for (const file of components) {
+        if (file.isFile() && path.extname(file['name']) === '.html') {
+          const stream = fs.createReadStream(path.join(componentsSource, file['name']), 'utf-8');
+          let data = '';
+          let key = `{{${file['name'].split('.')[0]}}}`;
+          stream.on('data', (chunk) => {
+            data += chunk;
+          });
+          stream.on('end', () => {
+            componentsObject[key] = data;
+            res(componentsObject);
+          });
+          stream.on('error', rej);
+        }
+      }
+    });
+  }
+  await createComponentObject();
+  // create variable with code from template.html
+  let templateData = '';
+  const template = path.join(__dirname, 'template.html');
+  function createtemplateData() {
+    return new Promise((res, rej) => {
+      const stream = fs.createReadStream(template, 'utf-8');
+      let data ='';
+      stream.on('data', chunk => {
+        data += chunk;
+      });
+      stream.on('end', () => {
+        templateData = data;
+        res(templateData);
+      });
+      stream.on('error', rej);
+    });
+  }
+  await createtemplateData();
+  // replace code in template.html
+  for (let key in componentsObject) {
+    templateData = templateData.replace(key, componentsObject[key]);
+  }
+  //create index.html
+  try {
+    await fs.promises.unlink(indexHtmlTarget);
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err;
+  }
+  await fs.promises.open(indexHtmlTarget, 'w');
+  await fs.promises.writeFile(indexHtmlTarget, templateData);
+  console.log('__SUCCESSFUL!__');
+}
+
+buildPage();
